93e8e7907b6a74ec60665e1f907fb9aa5d6cd2d4
victorvinicius33 <victorvinicius33.vv@gmail.com>
2022-07-17 04:37:24 -0300

    mudando a validação da data nos schemas de registrar e atualizar transação de regex para um método do yup

--- src/validations/schemaRegisterTransaction.js
+++ src/validations/schemaRegisterTransaction.js
@@ -4,12 +4,8 @@ const schemaRegisterTransaction = yup.object().shape({
   description: yup.string(),
   amount: yup.number().required('É necessário informar o valor da transação.'),
   date: yup
-    .string()
-    .required('É necessário informar uma data.')
-    .matches(
-      /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
-      'Informe um formato de data válido.'
-    ),
+    .date('Informe um formato de data válido')
+    .required('É necessário informar uma data.'),
   transaction_type: yup
     .string()
     .required('É necessário informar o tipo da transação.')
